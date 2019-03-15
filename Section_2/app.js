1701fdd8705b739a8f16c9ca16e559f30227346d
Juan Huerta <juan.huerta@upu.int>
2019-03-15 15:49:28 +0100

    Finished first exercise - Lesson 16

--- Section_2/app.js
+++ Section_2/app.js
@@ -3,17 +3,22 @@ var vueArguments = {
   data: {
     title: "Hello VueJS!",
     compundText: "Hello VueJS!",
-    googleLink: "http://www.google.com"
+    googleLink: "http://www.google.com",
+    finishedLink: "<a href='http://www.google.com'>The Link</a>"
   },
   methods: {
-    timeOfNow: function() {
+    timeOfNow: function () {
       var now = new Date();
       return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
     },
-    getText: function() {
+    getText: function () {
       return "Some Text";
     },
-    changeTitle: function(event) {
+    complexObject: function () {
+      var myArray = ['1', '2'];
+      return [myArray[0], "This is a coplex object", "array of some values"];
+    },
+    changeTitle: function (event) {
       this.compundText =
         this.title + this.getText() + ":  " + event.target.value;
 
